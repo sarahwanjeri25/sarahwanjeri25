bd9c438d24e6a0ba6ed034d632ffff499a540b37
sarahwanjeri25 <wanjerims450@gmail.com>
2024-03-04 11:13:49 +0300

    java

---
+++ javascript/functions.js
@@ -0,0 +1,35 @@
+/**Functions in javascript 
+ * Functions are the main building blocks of the progra.
+ * We've already seen examples of built-in functions,like "alert(message)"
+ * But wecan create fuctions of our own as well.
+ * 
+ * Therefore in javascript , functions  are blocks of code that ca be defined and caleed to perform a specific task.
+ * 
+ * FUNCTION DECLARATIONS
+ * Function declarations define a ne=amed function using the function keyword.
+ * 
+ * Syntax
+ * 
+ *       function functionname(){
+ *          
+ *         }
+*/
+        //examples
+        function showMessage(){
+            alert("Hello Everyone")
+        }
+
+
+/**
+ * In the example above , The function keyword goes first then goes the name of the function
+ * then a list of parameters between the parenthesis (comma-separated,empty in the example above ,we'll see examples later)
+ * and finally the code of function , also named " the function body",between curly braces.
+ * 
+ *     function name (parameter1,parameter2,.....parametern){
+ *       //body
+ *       }
+ * 
+ * -> In the example above to display the message"Hello Everyone" we just have to call the function  name as seen below
+ */
+
+showMessage();
